6328a8ce0d93a4d95bbe6b219a097eda93d31f7b
Jaishu Agarwal <consultjaishu@gmail.com>
2024-02-23 23:10:22 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region     =  "us-west-1"
+  region     =  "us-east-1"
 }
 
 resource "aws_instance" "myec2" {
-    ami = "ami-079db87dc4c10ac91"
+    ami = "ami-0440d3b780d96b29d"
     #instance_type = "t2.micro"
     instance_type = var.instance_type    # Setting variable for "instance_type".
     tags = {
